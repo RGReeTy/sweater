e2f88450c03ca8946debe61009f97a78cadf55a6
RGReeTy <be.a.reety@gmail.com>
2020-10-04 17:55:39 +0300

    add javadoc

--- src/main/java/com/example/sweater/service/MailSender.java
+++ src/main/java/com/example/sweater/service/MailSender.java
@@ -6,6 +6,9 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+/**
+ * The type Mail sender.
+ */
 @Service
 public class MailSender {
     @Autowired
@@ -14,6 +17,13 @@ public class MailSender {
     @Value("${spring.mail.username}")
     private String username;
 
+    /**
+     * Send.
+     *
+     * @param emailTo the email to
+     * @param subject the subject
+     * @param message the message
+     */
     public void send(String emailTo, String subject, String message) {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setFrom(username);

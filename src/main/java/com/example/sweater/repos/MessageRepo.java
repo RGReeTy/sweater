b238848dc9424218f4c5c01d16196812a48ec6af
RGReeTy <be.a.reety@gmail.com>
2020-10-05 23:31:15 +0300

    add javadoc

--- src/main/java/com/example/sweater/repos/MessageRepo.java
+++ src/main/java/com/example/sweater/repos/MessageRepo.java
@@ -5,7 +5,16 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
+/**
+ * The interface Message repo.
+ */
 public interface MessageRepo extends CrudRepository<Message, Long> {
 
+    /**
+     * Find by tag list.
+     *
+     * @param tag the tag
+     * @return the list
+     */
     List<Message> findByTag(String tag);
 }

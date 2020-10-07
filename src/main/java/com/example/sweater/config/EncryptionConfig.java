f4dc45f586f52430aa53bc444a64f7bddca9bc50
RGReeTy <be.a.reety@gmail.com>
2020-10-07 23:26:32 +0300

    add java doc

--- src/main/java/com/example/sweater/config/EncryptionConfig.java
+++ src/main/java/com/example/sweater/config/EncryptionConfig.java
@@ -5,9 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+/**
+ * The type Encryption config.
+ */
 @Configuration
 public class EncryptionConfig {
 
+    /**
+     * Gets password encoder.
+     *
+     * @return the password encoder
+     */
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder(8);

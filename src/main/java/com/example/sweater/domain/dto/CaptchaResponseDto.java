4f871d2c68e3c6f415758e9867fcaa1dd4930e4e
RGReeTy <be.a.reety@gmail.com>
2020-10-12 22:04:23 +0300

    add javadoc

--- src/main/java/com/example/sweater/domain/dto/CaptchaResponseDto.java
+++ src/main/java/com/example/sweater/domain/dto/CaptchaResponseDto.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.util.Set;
 
+/**
+ * The type Captcha response dto.
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CaptchaResponseDto {
 
@@ -12,18 +15,38 @@ public class CaptchaResponseDto {
     @JsonAlias("error-codes")
     private Set<String> errorCode;
 
+    /**
+     * Is success boolean.
+     *
+     * @return the boolean
+     */
     public boolean isSUCCESS() {
         return SUCCESS;
     }
 
+    /**
+     * Sets success.
+     *
+     * @param SUCCESS the success
+     */
     public void setSUCCESS(boolean SUCCESS) {
         this.SUCCESS = SUCCESS;
     }
 
+    /**
+     * Gets error code.
+     *
+     * @return the error code
+     */
     public Set<String> getErrorCode() {
         return errorCode;
     }
 
+    /**
+     * Sets error code.
+     *
+     * @param errorCode the error code
+     */
     public void setErrorCode(Set<String> errorCode) {
         this.errorCode = errorCode;
     }

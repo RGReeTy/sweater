3393ce9648822606a950c830d81ea89964d17d9a
RGReeTy <const.hsky@gmail.com>
2020-10-14 17:08:54 +0300

    add javadoc

--- src/main/java/com/example/sweater/repos/UserRepo.java
+++ src/main/java/com/example/sweater/repos/UserRepo.java
@@ -3,9 +3,24 @@ package com.example.sweater.repos;
 import com.example.sweater.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * The interface User repo.
+ */
 public interface UserRepo extends JpaRepository<User, Long> {
 
+    /**
+     * Find by username user.
+     *
+     * @param username the username
+     * @return the user
+     */
     User findByUsername(String username);
 
+    /**
+     * Find by activation code user.
+     *
+     * @param code the code
+     * @return the user
+     */
     User findByActivationCode(String code);
 }

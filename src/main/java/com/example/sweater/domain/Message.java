602787aac6e983829e5a63b863dc3aa3cc3ef9d0
RGReeTy <be.a.reety@gmail.com>
2020-10-16 21:37:25 +0300

    add javadoc

--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -5,6 +5,9 @@ import org.hibernate.validator.constraints.Length;
 import javax.persistence.*;
 import javax.validation.constraints.*;
 
+/**
+ * The type Message.
+ */
 @Entity
 public class Message {
 
@@ -25,55 +28,120 @@ public class Message {
 
     private String filename;
 
+    /**
+     * Instantiates a new Message.
+     */
     public Message() {
     }
 
+    /**
+     * Instantiates a new Message.
+     *
+     * @param text the text
+     * @param tag  the tag
+     * @param user the user
+     */
     public Message(String text, String tag, User user) {
         this.text = text;
         this.tag = tag;
         this.author = user;
     }
 
+    /**
+     * Gets author name.
+     *
+     * @return the author name
+     */
     public String getAuthorName() {
         return author != null ? author.getUsername() : "<none>";
     }
 
+    /**
+     * Gets author.
+     *
+     * @return the author
+     */
     public User getAuthor() {
         return author;
     }
 
+    /**
+     * Sets author.
+     *
+     * @param author the author
+     */
     public void setAuthor(User author) {
         this.author = author;
     }
 
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Gets text.
+     *
+     * @return the text
+     */
     public String getText() {
         return text;
     }
 
+    /**
+     * Sets text.
+     *
+     * @param text the text
+     */
     public void setText(String text) {
         this.text = text;
     }
 
+    /**
+     * Gets tag.
+     *
+     * @return the tag
+     */
     public String getTag() {
         return tag;
     }
 
+    /**
+     * Sets tag.
+     *
+     * @param tag the tag
+     */
     public void setTag(String tag) {
         this.tag = tag;
     }
 
+    /**
+     * Gets filename.
+     *
+     * @return the filename
+     */
     public String getFilename() {
         return filename;
     }
 
+    /**
+     * Sets filename.
+     *
+     * @param filename the filename
+     */
     public void setFilename(String filename) {
         this.filename = filename;
     }
